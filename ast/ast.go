9bed084ea5ba23821897cbedcbf72b0272d9c988
MateaLukiccc <ml20200410@student.fon.bg.ac.rs>
2024-01-28 17:47:58 +0100

    added parsing of let statements

---
+++ ast/ast.go
@@ -0,0 +1,64 @@
+package ast
+
+import "Eclab/token"
+
+// every node in AST has to implement this interface,
+// which means that it has to provide a method that
+// returns the literal value of the associated token
+type Node interface {
+	TokenLiteral() string
+}
+
+// Interface for Statement Nodes
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Interface for Expression Nodes
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// root node of every AST our parser provides
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier // variable name
+	Value Expression  // right side expression
+}
+
+func (ls *LetStatement) statementNode() {
+
+}
+
+// returns literal for a given token
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {
+
+}
+
+// returns token literal from a given identifier
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
---
+++ parser/parser.go
@@ -0,0 +1,111 @@
+package parser
+
+import (
+	"Eclab/ast"
+	"Eclab/lexer"
+	"Eclab/token"
+	"fmt"
+)
+
+type Parser struct {
+	l         *lexer.Lexer // lexer used for lexical analysis
+	errors    []string
+	curToken  token.Token
+	peekToken token.Token
+}
+
+// creates Parser and sets its fields to default values
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+// returns encountered errors while parsing
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+// moves curToken and peekToken of the parser by one
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+// appends new error to parser while checking upcoming token type
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+// entry point of the parsing process,
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+// parsing statements based on current token type
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+	// its expencted that identificator is after let token
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	// let statement name is equal to variable name
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	// after identificator there should be a = sign followed by expression
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+// check if current token is of expected type
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// check if next token is of expected type and advances tokens by 1
+// if errir is encountered it is appended to parser error list
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
