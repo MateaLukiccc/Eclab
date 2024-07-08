20de4de4bb990d9f89ed71b864e59c7cd659b6a8
MateaLukiccc <ml20200410@student.fon.bg.ac.rs>
2024-07-08 15:36:01 +0200

    added parsing of return statements

--- ast/ast.go
+++ ast/ast.go
@@ -26,12 +26,25 @@ type Program struct {
 	Statements []Statement
 }
 
+// return <expression>
+type ReturnStatement struct {
+	Token       token.Token // the return token
+	ReturnValue Expression  // expression returned by return statement
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+// returns literal for a given token
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
 }
 
 type LetStatement struct {
--- parser/parser.go
+++ parser/parser.go
@@ -63,6 +63,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -89,6 +91,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	return stmt
 }
 
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
 // check if current token is of expected type
 func (p *Parser) curTokenIs(t token.TokenType) bool {
 	return p.curToken.Type == t
@@ -99,7 +112,7 @@ func (p *Parser) peekTokenIs(t token.TokenType) bool {
 }
 
 // check if next token is of expected type and advances tokens by 1
-// if errir is encountered it is appended to parser error list
+// if error is encountered it is appended to parser error list
 func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
